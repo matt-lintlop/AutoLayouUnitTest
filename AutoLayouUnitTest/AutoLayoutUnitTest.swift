7fdef8703c7169924231d5345b4f3de064073453
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-07-05 02:12:34 -0700

    Implemented validateAutoLayoutForView()

--- AutoLayouUnitTest/AutoLayoutUnitTest.swift
+++ AutoLayouUnitTest/AutoLayoutUnitTest.swift
@@ -8,37 +8,40 @@
 
 import UIKit
 import QuartzCore
+import Foundation
 
 // Validate the auto-layout of a view. Returns true if valid.
-func validateAutoLayoutForView(_ view: UIView) -> Bool {
-    guard view.hasAmbiguousLayout == true else {
-        // the view's auto layout is valid
-        print("View AutoLayout Is Valid: \(view.accessibilityIdentifier ?? "?")")
-        return true
-    }
-    let horizontalConstraints = view.constraintsAffectingLayout(for: .horizontal)
-    horizontalConstraints.forEach { (constraint) in
-        print("H constraint for \(view.accessibilityIdentifier ?? "?"): \(constraint)")
-    }
-    let verticalConstraints = view.constraintsAffectingLayout(for: .vertical)
-    verticalConstraints.forEach { (constraint) in
-        print("V constraint for \(view.accessibilityIdentifier ?? "?"): \(constraint)")
-    }
-
-    view.exerciseAmbiguityInLayout()
-    CATransaction.flush()
-    
-    print("View AutoLayout Is Invalid: \(view.accessibilityIdentifier ?? "?")")
-    return true
-}
+func validateAutoLayoutForView(_ view: UIView)  {
+    DispatchQueue.main.async {
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        
+        guard view.hasAmbiguousLayout == true else {
+            // the view's auto layout is valid
+            assertionFailure("View AutoLayout Is Valid: \(view.accessibilityIdentifier ?? "?")")
+            return
+        }
+        let horizontalConstraints = view.constraintsAffectingLayout(for: .horizontal)
+        horizontalConstraints.forEach { (constraint) in
+            assertionFailure("H constraint for view ID = \(view.accessibilityIdentifier ?? "?"): \(constraint)")
+        }
+        let verticalConstraints = view.constraintsAffectingLayout(for: .vertical)
+        verticalConstraints.forEach { (constraint) in
+            assertionFailure("V constraint for view ID = \(view.accessibilityIdentifier ?? "?"): \(constraint)")
+        }
+        
+        view.exerciseAmbiguityInLayout()
+        CATransaction.flush()
+        
+        assertionFailure("ERROR: View AutoLayout Is Invalid: \(view.accessibilityIdentifier ?? "?")")
+  }
 
 // Validate the auto-layout of a list of views. Returns true if valid.
 func validateAutoLayoutForViews(_ views: [UIView]) -> Bool {
     var result = true
     views.forEach { (view) in
-        if validateAutoLayoutForView(view) == false {
-            result = false
-        }
+        validateAutoLayoutForView(view)
     }
     return result
+    }
 }
