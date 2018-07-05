7e050952b498b1fedf88ab8cc6490d6678e8ae74
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-07-05 02:49:21 -0700

    Debugged AutoLayoutUnitTest class. It works!

--- AutoLayouUnitTest/ViewController.swift
+++ AutoLayouUnitTest/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        testViews = [blueView, greenView, blueView]
+        testViews = [blueView, greenView, redView]
 }
 
 
