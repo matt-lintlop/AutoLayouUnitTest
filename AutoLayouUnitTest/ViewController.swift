908e137b4f38bb877c7d1d75915a52809f496aa9
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-07-05 01:40:30 -0700

    Initial Commit

---
+++ AutoLayouUnitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayouUnitTest
+//
+//  Created by Matt Lintlop on 7/5/18.
+//  Copyright © 2018 Matt Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
